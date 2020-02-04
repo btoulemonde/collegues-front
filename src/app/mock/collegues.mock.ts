e591a02e379056af4ad741d496be59fcd68af48e
Baptiste TOULEMONDE <baptistetoul@gmail.com>
2020-02-04 09:07:00 +0100

    #2

--- src/app/mock/collegues.mock.ts
+++ src/app/mock/collegues.mock.ts
@@ -1,4 +1,4 @@
-import {Collegue} from "../models/collegue";
+import {Collegue} from "../models/Collegue";
 
 let collegueMock:Collegue = new Collegue('M01','Dupont','Paul','paul.dupont@gmail.com',new Date(1990,4,5),'http://www.w3schools.com/bootstrap/img_avatar3.png');
 
