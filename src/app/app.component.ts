ed0d75a34e6c3bfcad76bbd9e0529e7a2403bdb6
Baptiste TOULEMONDE <baptistetoul@gmail.com>
2020-02-04 16:04:24 +0100

    corr

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {collegueMock} from './mock/collegues.mock';
+import { listeMatricules } from './mock/matricules.mock';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,4 +9,5 @@ import {collegueMock} from './mock/collegues.mock';
 export class AppComponent {
   title = 'collegues-front';
   unObjetCollegueFourni = collegueMock;
+  matriculeFourni = listeMatricules;
 }
